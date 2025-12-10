111977bd3502f81f3a4770d4d36c01c638d8e2ae
K <k@gmail.com>
2025-12-10 15:03:08 +0530

    laptop practice files merge

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args) {
+        
+        int num1 = 3;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
