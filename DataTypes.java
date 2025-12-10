957ded99cedf3cef69514227f6b3c856da31e3d8
K <101338230+kk-codes@users.noreply.github.com>
2025-12-10 15:03:08 +0530

    laptop practice files merge

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num = 58;
+        byte by = 127;
+        short sh = 23455;
+        char c = 'k';
+        long ln = 13414115L;
+        float fl = 5.8F;
+        double db = 5.8;
+        boolean bool = true;
+        System.out.println(num+":"+by+":"+sh+":"+c+":"+ln+":"+fl+":"+db+":"+bool);
+    }
+}
---
+++ Literal.java
@@ -0,0 +1,11 @@
+public class Literal {
+    public static void main(String[] args) {
+        int bi = 0b111;
+        int hex = 0x677;
+        int tenMillion = 1_00_00_000;
+        System.out.println(bi);
+        System.out.println(hex);
+        System.out.println(tenMillion);        
+
+    }
+}
